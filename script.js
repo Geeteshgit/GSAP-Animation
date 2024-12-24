0a07c452a9eaf8e601b8a59b9bff0fb593f8aa43
Geeteshgit <143749482+Geeteshgit@users.noreply.github.com>
2024-12-24 22:40:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// const lenis = new Lenis({
+//     lerp : 0.1,
+//     duration : 1
+// });
+// function raf(time) {
+//   lenis.raf(time);
+//   requestAnimationFrame(raf);
+// }
+// requestAnimationFrame(raf);
+
+// var tl = gsap.timeline();
+
+// tl.from(".hero-text h1",{
+//     y:10,
+//     opacity:0,
+//     duration:0.3,
+//     delay:1,
+// })
+// tl.from(".hero-text p",{
+//     y:10,
+//     opacity:0,
+//     duration:0.3,
+// })
+// tl.from(".hero-button",{
+//     y:10,
+//     opacity:0,
+//     duration:0.3,
+// })
+// tl.from(".social-links",{
+//     x:300,
+//     opacity:0,
+//     duration:0.3,
+//     stagger : 0.2
+// })
